a7d39d0e1747319fb68d9dde416daa652abc054f
Alan Van Omen <alanjvano@gmail.com>
2019-11-12 23:34:06 -0600

    comp filter - /0 error and gyro drift

--- src/hexa.py
+++ src/hexa.py
@@ -11,6 +11,7 @@ import argparse
 import numpy as np
 import curses
 from curses import wrapper
+import warnings
 
 # argument parser
 parser = argparse.ArgumentParser(description="hexa")
@@ -18,10 +19,9 @@ parser.add_argument('-c', '--conf', required=True, help='path to json config fil
 args = vars(parser.parse_args())
 print(args)
 
-# Variables
-gyro_pos = [0,0, 0]  # pitch (x), roll (y), yaw(z)
-gyro_sensitivity = 65.536
-acc_sensitivity = 8192.0
+# deal with warnings and load config 
+warnings.filterwarnings("ignore")
+conf = json.load(open(args["conf"]))
 
 #IMU_REFRESH = 
 #print("name: " + __name__)
@@ -59,6 +59,11 @@ class hexacopter:
         self.pos_comp = [0,0,0]     # complementary filter pos
         self.comp = [0,0,0]         # compass pos
         self.last_update = time.monotonic()      # last update time
+        self.gyro_sensitivity = float(conf["gyro_sensitivity"])
+        self.acc_sensitivity = 1 - self.gyro_sensitivity
+
+        # calibrated values
+        self.gyro_bias = [0,0,0]
 
     # update object values with data from IMU
     def update(self):
@@ -66,22 +71,26 @@ class hexacopter:
             # read data from IMU
             data = imu.getIMUData()
         
-            # convert to degrees
-            self.gyro_raw = np.degrees(data['gyro'])
-            self.pos_fus = np.degrees(data['fusionPose'])
-            self.pos_fus_q = np.degrees(data['fusionQPose'])
+            self.gyro_raw = data['gyro']
+            self.pos_fus = data['fusionPose']
+            self.pos_fus_q = data['fusionQPose']
             self.acc_raw = data['accel']
-            self.comp = np.degrees(data['compass'])
+            self.comp = data['compass']
+
+            # update complementary filter
+            [self.pos_comp, self.last_update] = complementary_filter(self.pos_comp, self.gyro_raw, self.acc_raw,
+                    self.gyro_sensitivity, self.acc_sensitivity, self.last_update, self.gyro_bias)
         
             time.sleep(poll_interval * 1.0/1000.0)
     
     # display hopefully useful info
     def disp(self, stdscr):
         stdscr.erase()
-        stdscr.addstr(1,0,'gyro    - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(self.gyro_raw))
-        stdscr.addstr(2,0,'accel   - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(self.acc_raw))
-        stdscr.addstr(3,0,'fusion  - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(self.pos_fus))
-        stdscr.addstr(4,0,'fusionq - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(self.pos_fus_q))
+        stdscr.addstr(1,0,'gyro    - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(np.degrees(self.gyro_raw)))
+        stdscr.addstr(2,0,'accel   - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(np.degrees(self.acc_raw)))
+        stdscr.addstr(3,0,'fusion  - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(np.degrees(self.pos_fus)))
+        stdscr.addstr(4,0,'fusionq - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(np.degrees(self.pos_fus_q)))
+        stdscr.addstr(5,0,'complem - x: {0[0]:.2f}  y: {0[1]:.2f}  z: {0[2]:.2f}'.format(np.degrees(self.pos_comp)))
         stdscr.refresh()
 
     def gyro_calibrate(self):
@@ -100,14 +109,27 @@ def main(stdscr):
         hexa.disp(stdscr)
 
     
-def complementary_filter(gyro_raw, acc_raw, gryo_sensitivity, acc_sensitivity, time_init):
-    tmp_pos = [0,0,0]
+def complementary_filter(pos_comp, gyro_raw, acc_raw, 
+                         gyro_sensitivity, acc_sensitivity, time_init, gyro_bias):
+    tmp_gyro = [0,0,0]
+    tmp_acc = [0,0,0]
 
    # gyroscope data returns only change in pos
    # p = integral(dp/dt)
    # because this is a discrete case, just sum change times time elapsed
-    for i in range(len(gyro_raw)):
-        tmp_pos += gyro_raw * (time.monotonic - time_init)
+    delta_t = time.monotonic() - time_init
+    for i, each in enumerate(tmp_gyro):
+        each = (gyro_raw[i] + gyro_bias[i]) * delta_t
+   
+    # based on Freesccale Semiconductor Application Note
+    # (www.nxp.com/doc/en/application-note/AN3461.pdf)
+    tmp_acc[0] = math.atan(acc_raw[1]/acc_raw[2])
+    tmp_acc[1] = math.atan(-1*acc_raw[0]/(acc_raw[1]**2+acc_raw[2]**2)**0.5)
+
+    pos_comp += gyro_sensitivity * np.asarray(tmp_gyro) + acc_sensitivity * np.asarray(tmp_acc)
+
+    # return updated position vector and last sample time
+    return pos_comp, time.monotonic()
 
 
 if __name__ == "__main__":
