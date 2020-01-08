66f64202007ab9c011970ae1425135250c2b5244
Alan Van Omen <alanjvano@gmail.com>
2020-01-07 23:20:00 -0700

    test np median

--- src/hexa.py
+++ src/hexa.py
@@ -316,6 +316,7 @@ def read_imu(stdscr, logger, poll_interval):
                     logger.debug('accel_hist roll ({}): {}'.format(i, accel_hist[i]))
                     each[0] = imu.get().accel_filtered[i]
                     logger.debug('accel_hist update ({}): {}'.format(i, each))
+                    logger.debug('numpy median ({}): {}'.format(i, np.median(accel_hist[i], axis=1)))
                     logger.debug('accel_ hist sort ({}):'.format(i, np.sort(each)))
                     imu.get().accel_filtered[i] = np.sort(each)[int(size/2.0)]
                     logger.debug('accel_hist after ({}): {}'.format(i, accel_hist[i]))
